245ad76a07389bad3caa3aedd399f8c3daadc57c
Nicolas Guarin-Zapata <nicoguarin@gmail.com>
2018-04-16 21:26:36 -0500

    Build documentation with Sphinx
    - Solves #5.
    - Docs available at [readthedocs](solidspy.readthedocs.io).

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,6 +3,12 @@
 History
 -------
 
+1.0.12 (2018-04-16)
+~~~~~~~~~~~~~~~~~~
+
+* Documentation built with Sphinx.
+* Docs in readthedocs.
+
 1.0.0 (2017-07-17)
 ~~~~~~~~~~~~~~~~~~
 
--- setup.py
+++ setup.py
@@ -13,17 +13,16 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='solidspy',
 
-    version='1.0.11a',
+    version='1.0.12',
 
     description='A simple Finite Element program',
     long_description=long_description,
-    long_description_content_type="text/markdown",
 
     # The project's main homepage.
     url='https://github.com/AppliedMechanics-EAFIT/SolidsPy',
