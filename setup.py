26c991f2ecf7104be9614fb3fe29691ce383d2eb
Nicolas Guarin-Zapata <nicoguarin@gmail.com>
2018-04-17 11:18:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
- # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """A simple Finite Element program
 
 https://github.com/AppliedMechanics-EAFIT/SolidsPy
@@ -16,6 +17,12 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+requirements = ['numpy',
+                'scipy',
+                'matplotlib',
+                'easygui',
+                'meshio']
+
 setup(
     name='solidspy',
 
@@ -23,6 +30,7 @@ setup(
 
     description='A simple Finite Element program',
     long_description=long_description,
+    long_description_content_type="text/x-rst",
 
     # The project's main homepage.
     url='https://github.com/AppliedMechanics-EAFIT/SolidsPy',
@@ -67,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'matplotlib', 'easygui', 'meshio'],
+    install_requires=requirements,
 
 
     # To provide executable scripts, use entry points in preference to the
