8b1ddaf68ed269a22c8068c844a996d13a0ab604
Nicolas Guarin-Zapata <nicoguarin@gmail.com>
2018-05-07 17:04:01 -0500

    Add citation to __init__.py

--- solidspy/__init__.py
+++ solidspy/__init__.py
@@ -10,3 +10,16 @@ __all__ = ["assemutil",
            "solutil",
            "uelutil",
            "solids_GUI"]
+
+
+__citation__ = """@software{solidspy,
+ title = {SolidsPy: 2D-Finite Element Analysis with Python},
+ author = {Gómez, Juan and Guarín-Zapata, Nicolás},
+ year = 2018,
+ keywords = {Python, Computer algebra system, Symbolics},
+ abstract = {SolidsPy is a simple finite element analysis code for
+   2D elasticity problems. The code uses as input simple-to-create text
+   files defining a model in terms of nodal, element, material and
+   load data.},
+ url = {https://github.com/AppliedMechanics-EAFIT/SolidsPy}
+}"""
