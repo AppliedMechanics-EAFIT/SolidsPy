83025855ef7c8627c91aaa5dc90a6fdffd25f344
Nicolas Guarin-Zapata <nicoguarin@gmail.com>
2018-05-09 17:17:57 -0500

    Version change in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements = ['numpy',
 setup(
     name='solidspy',
 
-    version='1.0.14',
+    version='1.0.15',
 
     description='A simple Finite Element program',
     long_description=long_description,
