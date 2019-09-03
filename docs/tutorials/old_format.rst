fd35f89fb879a182a83bf9232a1812291c260a66
Nicolas Guarin-Zapata <nicoguarin@gmail.com>
2019-09-03 12:14:51 -0500

    Add solids_auto function

---
+++ docs/tutorials/old_format.rst
@@ -0,0 +1,63 @@
+Using the old format format for SolidsPy
+========================================
+
+:Author: Nicolás Guarín-Zapata
+:Date: September 2019
+
+Until version 1.0.16 the data for a simulation was stored in 4 files
+
+- ``nodes.txt``
+
+- ``eles.txt``
+
+- ``mater.txt``
+
+- ``loads.txt``
+
+As shown below.
+
+- ``nodes.txt``
+
+::
+
+    0  0.00  0.00   0  -1
+    1  2.00  0.00   0  -1
+    2  2.00  2.00   0   0
+    3  0.00  2.00   0   0
+    4  1.00  0.00  -1  -1
+    5  2.00  1.00   0   0
+    6  1.00  2.00   0   0
+    7  0.00  1.00   0   0
+    8  1.00  1.00   0   0
+
+- ``eles.txt``
+
+::
+
+    0   1   0   0   4   8   7
+    1   1   0   4   1   5   8
+    2   1   0   7   8   6   3
+    3   1   0   8   5   2   6
+
+- ``mater.txt``
+
+::
+
+    1.0  0.3
+
+- ``loads.txt``
+
+::
+
+    3  0.0  1.0
+    6  0.0  2.0
+    2  0.0  1.0
+
+Run it in Python as follows:
+
+.. code:: python
+
+    import matplotlib.pyplot as plt  # load matplotlib
+    from solidspy import solids_GUI  # import our package
+    disp = solids_GUI()  # run the Finite Element Analysis
+    plt.show()    # plot contours
--- solidspy/solids_GUI.py
+++ solidspy/solids_GUI.py
@@ -99,6 +99,79 @@ def solids_GUI(plot_contours=True, compute_strains=False, folder=None):
         return disp_complete
 
 
+
+def solids_auto(data, plot_contours=True, compute_strains=False):
+    """
+    Run a complete workflow for a Finite Element Analysis
+
+    Parameters
+    ----------
+    data : dict
+        Simulation data composed of nodes, constrains, elements,
+        materials and loads.
+    plot_contours : Bool (optional)
+        Boolean variable to plot contours of the computed variables.
+        By default it is True.
+    compute_strains : Bool (optional)
+        Boolean variable to compute Strains and Stresses at nodes.
+        By default it is False.
+
+    Returns
+    -------
+    UC : ndarray (nnodes, 2)
+        Displacements at nodes.
+    E_nodes : ndarray (nnodes, 3), optional
+        Strains at nodes. It is returned when `compute_strains` is True.
+    S_nodes : ndarray (nnodes, 3), optional
+        Stresses at nodes. It is returned when `compute_strains` is True.
+
+    """
+    # Retrieving data
+    nodes = data["nodes"]
+    cons = data["cons"]
+    elements = data["elements"]
+    mats = data["mats"]
+    loads = data["loads"]
+
+
+    # Pre-processing
+    assem_op, bc_array, neq = ass.DME(cons, elements)
+    print("Number of nodes: {}".format(nodes.shape[0]))
+    print("Number of elements: {}".format(elements.shape[0]))
+    print("Number of equations: {}".format(neq))
+
+    # System assembly
+    stiff_mat, _ = ass.assembler(elements, mats, nodes, neq, assem_op)
+    rhs_vec = ass.loadasem(loads, bc_array, neq)
+
+    # System solution
+    start_time = datetime.now()
+    disp = sol.static_sol(stiff_mat, rhs_vec)
+    if not np.allclose(stiff_mat.dot(disp)/stiff_mat.max(),
+                       rhs_vec/stiff_mat.max()):
+        print("The system is not in equilibrium!")
+    end_time = datetime.now()
+    print('Duration for system solution: {}'.format(end_time - start_time))
+
+    # Post-processing
+    start_time = datetime.now()
+    disp_complete = pos.complete_disp(bc_array, nodes, disp)
+    strain_nodes, stress_nodes = None, None
+    if compute_strains:
+        strain_nodes, stress_nodes = pos.strain_nodes(nodes, elements, mats,
+                                                      disp_complete)
+    if plot_contours:
+        pos.fields_plot(elements, nodes, disp_complete, E_nodes=strain_nodes,
+                        S_nodes=stress_nodes)
+    end_time = datetime.now()
+    print('Duration for post processing: {}'.format(end_time - start_time))
+    print('Analysis terminated successfully!')
+    if compute_strains:
+        return (disp_complete, strain_nodes, stress_nodes)
+    else:
+        return disp_complete
+
+
 if __name__ == '__main__':
     displacement = solids_GUI()
     plt.show()
