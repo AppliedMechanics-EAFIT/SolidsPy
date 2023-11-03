21c3fc2531580a4739c1da8a4e70f67e61fc7fe2
Nicolás Guarín-Zapata <nicoguarin@gmail.com>
2023-11-03 14:41:40 -0500

    Change README to MarkDown
    - To make it compatible with PyPI we changed to MarkDown.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 requirements = ['numpy',
@@ -30,7 +30,7 @@ setup(
 
     description='A simple Finite Element program',
     long_description=long_description,
-    long_description_content_type="text/x-rst",
+    long_description_content_type="text/markdown",
 
     # The project's main homepage.
     url='https://github.com/AppliedMechanics-EAFIT/SolidsPy',
