e66c71e507cfa4c862a5b62ef60faf86b13209e0
Nicolás Guarín-Zapata <nicoguarin@gmail.com>
2023-11-04 09:51:38 -0500

    Update version in __init__.py

--- solidspy/__init__.py
+++ solidspy/__init__.py
@@ -11,13 +11,13 @@ __all__ = ["assemutil",
            "uelutil",
            "solids_GUI"]
 
-__version__ = "1.0.16"
+__version__ = "1.1.0"
 
 
 __citation__ = """@software{solidspy,
  title = {SolidsPy: 2D-Finite Element Analysis with Python},
- author = {Gómez, Juan and Guarín-Zapata, Nicolás},
- year = 2018,
+ author = {Guarín-Zapata, Nicolás and Gómez, Juan},
+ year = 2023,
  keywords = {Python, Finite elements, Scientific computing, Computational mechanics},
  abstract = {SolidsPy is a simple finite element analysis code for
    2D elasticity problems. The code uses as input simple-to-create text
