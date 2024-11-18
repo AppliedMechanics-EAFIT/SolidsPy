15440aa5583ca3f2174cb7d95b7b27117297cfd6
kssgarcia <kssgarcia@outlook.com>
2024-11-18 13:45:11 -0500

    Fix version formatting in setup.py, conda-recipe/meta.yaml, and docs/conf.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements = ['numpy',
 setup(
     name='SolidsPyKevin',
 
-    version='1.2.8',
+    version="1.2.8",
 
     description='A simple Finite Element program',
     long_description=long_description,
