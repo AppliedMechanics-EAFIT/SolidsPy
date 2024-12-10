9f2c61eb18f0d99a399210108b4ce13108487c5e
kssgarcia <kssgarcia@outlook.com>
2024-12-10 11:43:18 -0500

    Remove redundant comment about adding src directory to Python path in Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@
 import os
 import sys
 
-# Add the src directory to Python path
 sys.path.insert(0, os.path.abspath('../src'))
 
 # -- General configuration ------------------------------------------------
