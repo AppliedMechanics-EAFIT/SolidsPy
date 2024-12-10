1504b0b462e25b37f38d7dc0f8063961ddaebc2d
kssgarcia <kssgarcia@outlook.com>
2024-12-10 11:03:08 -0500

    Update import statement in Sphinx configuration to use the package name directly

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../src/solidspy'))
 
-import src.solidspy
+import solidspy
 
 
 # -- General configuration ------------------------------------------------
